a9083feeed8da94168349caea9040f1eb9980004
federicopotrich <federico.potrich@marconirovereto.it>
2023-11-10 17:23:56 +0100

    fix

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +14,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
-            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up, ForceMode.Inpulse);
+        if(Input.GetKeyUp(KeyCode.Space)){
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up*7.5f, ForceMode2D.Impulse);
+            isJumping = true;
         }
+    }
+
+    void OnColliderExit2D(Collider2D coll){
+        isJumping = false;
     }
 }
